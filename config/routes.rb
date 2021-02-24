99587174c68181c8cab2b1a6d8630d5a67ff73c1
dmepilot <dmepilot@yahoo.com>
2021-02-24 12:49:37 -0700

    added welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :hikes
   resources :trails
   resources :users
+  get '/', to: 'static#welcome'
+  get 'about', to: 'static#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
