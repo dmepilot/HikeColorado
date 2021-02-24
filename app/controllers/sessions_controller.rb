3ab16e42ed17e170fec1f52c318e474ce662aa50
dmepilot <dmepilot@yahoo.com>
2021-02-24 15:36:45 -0700

    user show page

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+    def create
+        user = User.find_by(username: params[:user][:username])
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            redirect_to "/users/#{user.id}"
+        else
+            flash[:message] = "Incorrect login info, please try again"
+            redirect_to "/login"
+        end
+    end
+
+    def destroy
+        session.clear
+        redirect_to root_path
+    end
+
+end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,15 @@ Rails.application.routes.draw do
   resources :users
   get '/', to: 'static#welcome'
   get 'about', to: 'static#about'
+
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+ 
+  #login route
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+ 
+  #logout route
+  delete '/logout' => 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
