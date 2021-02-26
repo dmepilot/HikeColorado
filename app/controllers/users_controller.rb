4045e2b06be47f12d5dbd777e31d21b58e48ff2f
dmepilot <dmepilot@yahoo.com>
2021-02-26 13:42:39 -0700

    navbar styling and not logged in redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,21 @@
 class UsersController < ApplicationController
+    before_action :redirect_if_not_logged_in, :except=>[:new,:create]
+
 
     def new
+        @user = User.new
     end
 
     def create
+        # byebug
+        @user= User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            # byebug
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
     end
     
     
