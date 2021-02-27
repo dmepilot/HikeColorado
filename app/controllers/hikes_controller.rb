cd3e55d4db92cb405df4e3ae3a4b93fb85bf9dec
dmepilot <dmepilot@yahoo.com>
2021-02-26 22:17:28 -0700

    show pages

--- app/controllers/hikes_controller.rb
+++ app/controllers/hikes_controller.rb
@@ -1,2 +1,31 @@
 class HikesController < ApplicationController
+
+    def new
+        @hike = Hike.new
+    end
+
+    def create
+        # byebug
+        @hike = Hike.create(hike_params)
+        if @hike.save
+            redirect_to hike_path(@hike)
+        else
+            redirect_to new_hike_path
+        end
+    end
+
+    def show
+        @hike = Hike.find_by(id: params[:id])
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    private
+    def hike_params
+        params.require(:hike).permit(:date, :rating, :comments, :trail_id, :user_id)
+    end
 end
