ee9f2647a231b45835e2548928f8d38020028f06
dmepilot <dmepilot@yahoo.com>
2021-03-07 09:02:22 -0700

    trails index view header

--- app/helpers/hikes_helper.rb
+++ app/helpers/hikes_helper.rb
@@ -1,2 +1,14 @@
 module HikesHelper
+
+    def index_display
+        if params[:user_id]
+            user=User.find(params[:user_id])
+           "#{user.name}'s Hikes"
+        elsif params[:trail_id]
+            trail=Trail.find(params[:trail_id])
+            "#{trail.name} Hikes"
+        else
+            "All Hikes"
+        end
+    end
 end
