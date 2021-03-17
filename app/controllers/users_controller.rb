8813c7a1df323eff2b31af77d03bd4c37c8bf716
dmepilot <dmepilot@yahoo.com>
2021-03-17 15:57:51 -0600

    added user edit validation messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,17 +20,17 @@ class UsersController < ApplicationController
     
     
     def show
-        # @user = User.find_by(id: params[:id])  
     end
 
     def edit
-    #   @user = User.find_by(id: params[:id])
     end
 
     def update
-        # user = User.find_by(id: params[:id])
-        @user.update(user_params)
-        redirect_to user_path(@user)
+        if @user.update(user_params)
+          redirect_to user_path(@user)
+        else
+            render :edit
+        end
     end
 
 
